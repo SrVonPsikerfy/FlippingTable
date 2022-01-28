50a438c29a3a8ea0579cc2630584ff39c6f80f19
Juandi <juandiem@ucm.es>
2022-01-29 00:34:50 +0100

    Control Input Rotacion mapa

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,20 +6,26 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField]
     int size; 
+    
     public GameObject prefabW = null;
 
     public GameObject prefabB = null;
+    [Header("RotateSpeed")]
+    public float rSpeed = 5.0f;
     // Start is called before the first frame update
     void Start(){
         float init = -size/2;
 
         float auxX = init, auxZ = init;
         bool alternate = true;
+        GameObject cases = null;
         if(prefabB != null && prefabW != null ){
             for(int i = 0; i < size; i++){
                 for(int j = 0; j < size; j++){
-                    if(alternate) Instantiate(prefabB, new Vector3(auxX, 0 , auxZ), Quaternion.identity);    
-                    else Instantiate(prefabW, new Vector3(auxX, 0 , auxZ), Quaternion.identity);
+                    if(alternate) 
+                        cases = Instantiate(prefabB, new Vector3(auxX, 0 , auxZ), Quaternion.identity);    
+                    else 
+                        cases = Instantiate(prefabW, new Vector3(auxX, 0 , auxZ), Quaternion.identity);
 
                     auxZ++;
                     alternate = !alternate;
@@ -33,7 +39,16 @@ public class Spawner : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-
+            if (Input.GetKeyDown(KeyCode.F)){
+                Debug.Log("uwu");
+                rotateMap();
+            }
         
+    }
+
+    void rotateMap(){
+        Quaternion currentRotation = transform.rotation;
+        Quaternion wantedRotation = Quaternion.Euler(180,0,0);
+        transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * rSpeed);
     }
 }
