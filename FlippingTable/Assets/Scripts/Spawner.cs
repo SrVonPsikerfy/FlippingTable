8d2e41bd1375b84d35b171b44a9fe45619d9dfd9
joseda <jrave@ucm.es>
2022-01-28 22:39:18 +0100

    Primer Commit de esta cosa

---
+++ FlippingTable/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    int size; 
+    public GameObject prefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float init = size/2;
+
+        float auxX = init, auxZ = init;
+        if(prefab != null){
+            Debug.Log("Cosas");
+            for(int i = 0; i < size; i++){
+                for(int j = 0; j < size; j++){
+                    auxZ++;
+                    Instantiate(prefab, new Vector3(auxX, 0 , auxZ), Quaternion.identity);
+                }
+                auxX++;
+                auxZ = init;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        
+    }
+}
