8a438b2b619c5d79dc29c094d5181b1e1e086984
Juandi <juandiem@ucm.es>
2022-01-29 03:20:10 +0100

    Rotacion en ola del mapa

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -9,38 +9,41 @@ public class MapManager : MonoBehaviour
     [SerializeField]
     int size = GameManager.tableroSize; 
 
-    public GameManager gM; 
     public GameObject prefabW = null;
     public GameObject prefabB = null;
-    [Header("RotateSpeed")]
-    public float rSpeed = 5.0f;
+    [Header("delay")]
+    public float delay = 0.5f;
+    public float rSpeed = 50;
 
     //Privates
     Quaternion wantedRotation = Quaternion.Euler(180,0,0);
-    bool flipDone = true;
-    bool topSide = true;
+    bool flipDone = true;   //Ha terminado el flip
+    bool topSide = true;    //Esta cara Arriba
+    float countdown;
+    int k = 0;  //diagonal inicial
+
 
     void Start(){
-
-        if(gM == null){
-            Debug.Log("There is not GameManager");
-        }
-
+        countdown = delay;
         generateMap();
     }
 
     // Update is called once per frame
     void Update(){
-        if (Input.GetKeyDown(KeyCode.F)){
+        if (Input.GetKeyDown(KeyCode.F) && flipDone){
             Debug.Log("uwu");
             flipDone = false;
             topSide = !topSide;
             wantedRotation = (topSide)? Quaternion.Euler(0,0,0): Quaternion.Euler(180,0,0);
         }
-        if(GameManager.getCell(size-1,size-1).transform.rotation.x == wantedRotation.x)
+        if(GameManager.getCell(size-1,size-1).transform.rotation.x == wantedRotation.x){
             flipDone = true;
-            
+            k = 0;
+            countdown = delay;
+        }
+
         if(!flipDone) flipMap();
+        Debug.Log(k);
     }
 
     void generateMap(){
@@ -69,6 +72,7 @@ public class MapManager : MonoBehaviour
                     }    
                     else cases = Instantiate(prefabW, new Vector3(auxX, ss.y * (int)a , auxZ), Quaternion.identity);
 
+                    cases.transform.parent = this.transform;
                     Vector2 pos = new Vector2(j,i);
                     GameManager.addCell(pos, cases, a);
                     casillaInfo cas = cases.GetComponent<casillaInfo>();
@@ -86,12 +90,24 @@ public class MapManager : MonoBehaviour
     }
 
     void flipMap(){
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
+        countdown -= Time.deltaTime;
+        if(countdown <= 0){
+            countdown = delay;
+            k++;
+        }    
+        for(int i = k; i >= 0; i--)
+            flipDiagonal(i);
+    }
+
+    
+    void flipDiagonal(int k){
+        for(int j = 0 ; j <= k ; j++ ) {
+            int i = k - j;
+            if( i < size && j < size ) {
                 GameObject child = GameManager.getCell(i,j);
                 Quaternion currentRotation = child.transform.rotation;
                 child.transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * rSpeed);
             }
-        }        
+        }
     }
 }
